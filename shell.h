9c6029f319379f20f9faf6f5cfc41a5f1c0a3cee
pablonudel <pablonudel@gmail.com>
2024-12-19 14:44:27 +0100

    rename header file

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef shell_h
+#define shell_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+
+extern char **environ;
+
+char *get_input(char *buffer, size_t buffer_size);
+char **split_input(char *buffer, char *del);
+char *_getenv(const char *env_var);
+char *get_cmd(char *cmd);
+
+#endif
