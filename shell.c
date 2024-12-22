d73dcac784e75c758de8d8e52ed1e5dd26198c00
pablonudel <pablonudel@gmail.com>
2024-12-22 12:28:46 +0100

    checking command before fork and improving allocation mem frees

--- shell.c
+++ shell.c
@@ -1,88 +1,92 @@
 #include "shell.h"
-/**
- * main - 
- *
- * Return: Always 0.
- */
+
 int main(void)
 {
-	char *buffer = NULL, *cmd, **args;
-	char **env;
-	size_t buffer_size = 0;
+	char *input, **args, *command;
 	pid_t pid;
-	int status, i;
+	int status;
+	int i;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO) == 1)
 			write(1, "#cisfun$ ", 9);
 
-		buffer = get_input(buffer, buffer_size);
-
-		args = split_input(buffer, " \t\n");
+		input = get_input();
+		args = split_input(input);
 
 		if (!args)
 		{
-			free(buffer);
+			free(input);
 			continue;
 		}
 
-		if (args[0] == NULL)
+		if (!args[0])
 			continue;
 
 		if (strcmp(args[0], "exit") == 0)
 		{
+			free(input);
 			for (i = 0; args[i]; i++)
 				free(args[i]);
 			free(args);
-			free(buffer);
 			exit(EXIT_SUCCESS);
 		}
-
-		if (strcmp(args[0], "env") == 0)
+		else if (strcmp(args[0], "env") == 0)
 		{
 			if (args[1])
 			{
-				perror("Error");
+				perror("Error no env args");
+				free(input);
+				for (i = 0; args[i]; i++)
+					free(args[i]);
+				free(args);
 				continue;
 			}
-			env = environ;
-			while (*env)
-			{
-				printf("%s\n", *env);
-				env++;
-			}
-			continue;
-		}
-
-		pid = fork();
-
-		if (pid == -1)
-		{
-			perror("Error\n");
-			free(buffer);
-			exit (1);
-		}
-
-		if (pid == 0)
-		{
-			cmd = get_cmd(args[0]);
-			if (execve(cmd, args, environ) == -1)
-			{
-				printf("Error\n");
-				free(buffer);
-				exit(1);
-			}
-			exit(0);
+			print_env();
 		}
 		else
-			wait(&status);
+		{
+			command = get_cmd(args[0]);
+			if (!command)
+			{
+				fprintf(stderr, "%s: command not found\n", args[0]);
+				free(input);
+				for (i = 0; args[i]; i++)
+					free(args[i]);
+				free(args);
+				continue;
+			}
+
+			pid = fork();
+
+			if (pid == -1)
+			{
+				perror("Error en pid padre");
+			}
+			if (pid == 0)
+			{   
+				if (execve(command, args, environ) == -1)
+				{
+					perror("Error en pid hijo");
+					for (i = 0; args[i]; i++)
+						free(args[i]);
+					free(args);
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+				wait(&status);
+
+		}
+
 
 		for (i = 0; args[i]; i++)
 			free(args[i]);
 		free(args);
-		free(buffer);
+		free(input);
+		if (command)
+			free(command);
 	}
-
 	return (0);
 }
