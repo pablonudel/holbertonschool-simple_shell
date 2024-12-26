8e46bfa4e27ae5f77cafc6aa6bdd26f258de40af
pablonudel <pablonudel@gmail.com>
2024-12-26 11:39:52 +0100

    moving the command_handler function to main file for better readability

--- shell.c
+++ shell.c
@@ -14,7 +14,25 @@ int main(void)
 		prompt();
 		input = get_input();
 		args = split_input(input);
-		command_handler(input, args);
+
+		if (!args)
+		{
+			free(input);
+			continue;
+		}
+
+		if (!args[0])
+			continue;
+
+		if (strcmp(args[0], "exit") == 0)
+			builtin_exit(input, args);
+		else if (strcmp(args[0], "env") == 0)
+			builtin_env(args);
+		else
+			exec_cmd(args);
+
+		free_array(args);
+		free(input);
 	}
 	return (0);
 }
