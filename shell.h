d09bc3c0198b7a79bb033a27a55e58f5a3094c62
pablonudel <pablonudel@gmail.com>
2024-12-26 11:40:20 +0100

    removing unused prototype

--- shell.h
+++ shell.h
@@ -17,7 +17,6 @@ char *get_input(void);
 char **split_input(char *input);
 char *_getenv(const char *env_var);
 void free_array(char **array);
-void command_handler(char *input, char **args);
 char *get_cmd(char *arg);
 void exec_cmd(char **args);
 void builtin_exit(char *input, char **args);
