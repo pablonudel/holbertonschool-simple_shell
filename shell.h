1d38b92ec411671ae72fc53fb53885d30b296237
pablonudel <pablonudel@gmail.com>
2025-01-03 11:45:06 +0100

    prototype update

--- shell.h
+++ shell.h
@@ -17,7 +17,7 @@ char *get_input(void);
 char **split_input(char *input);
 char *_getenv(const char *env_var);
 void free_array(char **array);
-char *get_cmd(char *arg);
+char *get_cmd(char *arg, int *error);
 void exec_cmd(char **args);
 void builtin_exit(char *input, char **args);
 void builtin_env(char **args);
