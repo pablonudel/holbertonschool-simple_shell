988bb1aed97dee3efde5823c5d92fc91afcecb25
pablonudel <pablonudel@gmail.com>
2025-01-03 17:34:40 +0100

    handling eof fixed and preventing new lines at the end of input

--- input_handlers.c
+++ input_handlers.c
@@ -10,23 +10,33 @@ char *get_input(void)
 	char *input = NULL;
 	size_t input_size = 0;
 	ssize_t n_chars;
+	int i;
 
 	n_chars = getline(&input, &input_size, stdin);
+	if (n_chars == EOF)
+	{
+		free(input);
+		if (isatty(STDIN_FILENO))
+			putchar('\n');
+		exit(EXIT_SUCCESS);
+	}
 	if (n_chars == -1)
 	{
-		if (feof(stdin))
-		{
-			free(input);
-			exit(EXIT_SUCCESS);
-		}
-		else
-		{
-			perror("hsh: Error");
-			free(input);
-			exit(EXIT_FAILURE);
-		}
+		perror("hsh: Error");
+		free(input);
+		exit(EXIT_FAILURE);
 	}
 
+	i = 0;
+	while (input[i] != '\0')
+	{
+		if (input[i] == '\n')
+		{
+			input[i] = '\0';
+			break;
+		}
+		i++;
+	}
 	return (input);
 }
 
